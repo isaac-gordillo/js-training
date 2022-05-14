5149c987ff01052c94e1987eb70c5c442f9aa208
isaac-gordillo <isaac.gordillo@es.stroeer-labs.com>
2022-05-14 13:56:47 +0200

    init project

---
+++ index.js
@@ -0,0 +1,58 @@
+const API_BASE_URL = 'http://localhost:3001'
+const POKEMONS_URL = 'pokemons';
+
+async function main() {
+    const pokemons = await fetch(`${API_BASE_URL}/${POKEMONS_URL}`).then(res => res.json());
+    const pokemonTable = createTable(pokemons);
+    document.getElementById('pokemon-table').innerHTML = pokemonTable;
+}
+
+function createPokemonTable(pokemons) {
+    let table = `<table><tr><th>Name</th><th>Type</th><th>Image</th></tr>`;
+    pokemons.forEach(pokemon => {
+        table += `<tr><td>${pokemon.name}</td><td>${pokemon.type}</td><td class="pokemon-image"><img src="${pokemon.image}"/></td></tr>`;
+    });
+    table += '</table>';
+    
+    return table;
+}
+
+function createTable(items){
+    const columns = Object.keys(items[0]);
+    let table = `<table>`;
+    table += createTableHeaders(columns);
+
+    table += createTableRows(items, columns);
+
+    return table;
+}
+
+function createTableHeaders(columns){
+    let headers = `<tr>`;
+    columns.forEach(column => {
+        headers += `<th>${column}</th>`;
+    });
+    headers += '</tr>';
+
+    return headers;
+}
+
+function createTableRows(items, columns) {
+    let rows = '';
+    items.forEach(item => {
+        rows += '<tr>';
+        columns.forEach(column => {
+            if (column === 'image') {
+                rows += `<td style="text-align:center"><img src="${item[column]}"></td>`;
+            } else {
+                rows += `<td>${item[column]}</td>`;
+            }
+        });
+        rows += '</tr>';
+    });
+    rows += '</table>';
+    return rows;
+}
+
+
+main();
